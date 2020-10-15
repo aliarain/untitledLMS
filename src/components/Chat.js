4a8ac4e99985759b49240a296e07a77d75afb308
chamindujs <chamindujs@gmail.com>
2020-10-15 16:42:34 +0530

    Test emoji input

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react';
+import Picker from 'emoji-picker-react';
+ 
+const Chat = () => {
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+ 
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+  };
+ 
+  return (
+    <div>
+      {chosenEmoji ? (
+        <span>You chose: {chosenEmoji.emoji}</span>
+      ) : (
+        <span>No emoji Chosen</span>
+      )}
+      <Picker onEmojiClick={onEmojiClick} />
+    </div>
+  );
+};
 
-export default class Chat extends Component {
-    render() {
-        return (
-            <div className='routeArea'>
-                 hello this is chat
-            </div>
-        )
-    }
-}
+export default Chat
